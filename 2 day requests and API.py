dbd891b28e4369d59802cf8f4d79536a13d5fb8d
KolBbl4 <ko1ba@mail.ru>
2025-08-13 22:30:56 +0500

    API JSON

--- 2 day requests and API.py
+++ 2 day requests and API.py
@@ -1,13 +1,10 @@
 import requests 
 
-access_key = 'your_key'
 
-headers = {
-    'X-Yandex-Weather-Key': access_key
-}
-
-response = requests.get('https://api.weather.yandex.ru/v2/forecast?lat=52.37125&lon=4.89388', headers=headers)
+response = requests.get('https://jsonplaceholder.typicode.com/posts')
 if response.status_code == 200:
-    print(response.json())
+    users = response.json()
+    for user in users:
+        print(user['userId'])    
 else:
     print(response.status_code)
